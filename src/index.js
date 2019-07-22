65d4b021a95d67736f0be069ccb6418976a7b256
Dasol Park <dasol.park615@gmail.com>
2019-07-22 23:26:49 +0800

    21. Values JSX Can’t Show

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 // Import the React and ReactDOM libraries
-import React from "react";
-import ReactDOM from "react-dom";
-
-function getButtonText() {
-  return "Click Me!(function)";
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-  const buttonText = "Click Me!(Variable)";
+  const buttonText = { text: 'Click me' };
+  const labelText = 'Enter name:';
+  const style = { backgroundColor: 'blue', color: 'white' };
 
   return (
     <div>
       <label className="label" for="name">
-        Enter name:
+        {labelText}
       </label>
       <input id="name" type="text" />
-      <button style={{ backgroundColor: "blue", color: "white" }}>
-        {getButtonText()}
-        {buttonText}
-      </button>
+      <button style={style}>{buttonText.text}</button>
     </div>
   );
 };
 
 // Take the react component and show it on the screen
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
